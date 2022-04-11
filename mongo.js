b5c03bd7b1984dda68363e5bc07b948080113859
Daniel Redly <RedlyDP@cardiff.ac.uk>
2022-04-11 09:16:46 +0100

    Completed part 3d

--- mongo.js
+++ mongo.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument: node mongo.js <password>');
-    process.exit(1);
+	console.log('Please provide the password as an argument: node mongo.js <password>')
+	process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://notes:${password}@cluster0.be3y0.mongodb.net/noteapp?retryWrites=true&w=majority`;
-mongoose.connect(url);
+const url = `mongodb+srv://notes:${password}@cluster0.be3y0.mongodb.net/noteapp?retryWrites=true&w=majority`
+mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
+	content: String,
+	date: Date,
+	important: Boolean,
 })
 
-const Note = mongoose.model('Note', noteSchema);
+const Note = mongoose.model('Note', noteSchema)
 
 // const note = new Note({
 //     content: 'JS is weird',
@@ -30,8 +30,8 @@ const Note = mongoose.model('Note', noteSchema);
 // })
 
 Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log(note);
-    })
-    mongoose.connection.close();
+	result.forEach(note => {
+		console.log(note)
+	})
+	mongoose.connection.close()
 })
