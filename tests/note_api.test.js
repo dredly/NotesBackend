4f77890bad548b26e042272d88fc10aa8a7106a3
Daniel Redly <RedlyDP@cardiff.ac.uk>
2022-04-21 10:10:40 +0100

    Completed Section 4c

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 const supertest = require('supertest')
 const app = require('../app')
 const helper = require('./test_helper')
 const Note = require('../models/note')
+const User = require('../models/user')
 
 const api = supertest(app)
 
@@ -112,7 +114,51 @@ describe('deletion of a note', () => {
 	})
 })
 
+describe('when there is initially one user in the db', () => {
+	beforeEach(async () => {
+		await User.deleteMany({})
+		const passwordHash = await bcrypt.hash('miguel', 10)
+		const user = new User({ username: 'root', passwordHash })
+		await user.save()
+	})
 
+	test('creation succeeds with a fresh username', async () => {
+		const usersAtStart = await helper.usersInDb()
+		const newUser = {
+			username: 'zlatan123',
+			name: 'Zlatan Ibrahimovic',
+			password: 'zlatan'
+		}
+		await api
+			.post('/api/users')
+			.send(newUser)
+			.expect(201)
+			.expect('Content-Type', /application\/json/)
+
+		const usersAtEnd = await helper.usersInDb()
+		expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+		const usernames = usersAtEnd.map(u => u.username)
+		expect(usernames).toContain(newUser.username)
+	})
+
+	test('creation fails with proper status code and message if username taken', async () => {
+		const usersAtStart = await helper.usersInDb()
+		const newUser = {
+			username: 'root',
+			password: 'rootpass'
+		}
+		const result = await api
+			.post('/api/users')
+			.send(newUser)
+			.expect(400)
+			.expect('Content-Type', /application\/json/)
+
+		expect(result.body.error).toContain('username must be unique')
+		const usersAtEnd = await helper.usersInDb()
+		expect(usersAtEnd).toEqual(usersAtStart)
+	})
+})
 
 afterAll(() => {
 	mongoose.connection.close()
